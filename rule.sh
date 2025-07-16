d5a579add3577081c8296b992e1aff46462d0cb0
Jason <617712938@qq.com>
2025-07-16 10:17:09 +0800

    quick update

--- rule.sh
+++ rule.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
+#set -x
+# CI_COMMIT_TAG=1.0.0-alpha1
+# CI_COMMIT_BRANCH=1.0.0-alpha1
+GIT_DIFF=$(git diff --name-only HEAD^..HEAD | grep -E '^.*\.(cpp|hpp|c|h|cc)$')
+GIT_DIFF_TEST=$(git diff --name-only HEAD^..HEAD | grep -E '^(app_home/|src/|test/|include/|.*\.(cpp|hpp|c|h|cc)|CMakeLists\.txt)$')
+# GIT_DIFF=cpp
+# GIT_DIFF_TEST=cpp
+IMAGE_LIST_STR="arm-kylin-v10sp1 rhel7.9"
+IFS=' ' read -r -a IMAGE_LIST <<<"$IMAGE_LIST_STR"
+#IMAGE_LIST=($IMAGE_LIST_STR)
+echo "制作镜像版本: ${IMAGE_LIST[*]}"
+PARA_FILES="rule_before_integrate
+rule_tags_permit_scan
+rule_cppcheck_scan
+rule_x86_clang_tidy_scan
+rule_arm_clang_tidy_scan
+rule_x86_coverage
+rule_arm_kylin_coverage
+rule_spec_format
+rule_code_docs
+rule_rhel7.9_test
+rule_rhel8.3_test
+rule_rhel8.6_test
+rule_rhel8.10_test
+rule_rhel7.9_integrate
+rule_rhel8.3_integrate
+rule_rhel8.6_integrate
+rule_rhel8.10_integrate
+rule_x86_kylin_v7_test
+rule_x86_kylin_v10sp1_test
+rule_x86_kylin_v10sp3_test
+rule_x86_kylin_v7_integrate
+rule_x86_kylin_v10sp1_integrate
+rule_x86_kylin_v10sp3_integrate
+rule_arm_kylin_v10sp1_test
+rule_arm_kylin_v10sp3_test
+rule_arm_kylin_v10sp1_integrate
+rule_arm_kylin_v10sp3_integrate"
 
-# GIT_DIFF=$(git diff --name-only HEAD^..HEAD | grep -E '^.*\.(cpp|hpp|c|h|cc)$')
-# GIT_DIFF_TEST=$(git diff --name-only HEAD^..HEAD | grep -E '^(app_home/|src/|test/|include/|.*\.(cpp|hpp|c|h|cc)|CMakeLists\.txt)$')
+IFS=$'\n' read -r -d '' -a PARA_FILES_LIST <<<"$PARA_FILES"
 
-GIT_DIFF=cpp
-GIT_DIFF_TEST=app
-IMAGE_LIST="arm-kylin-v10 rhel8.6"
-IMAGE_LIST=($IMAGE_LIST)
-PARA_FILES=(
-"rule_rhel810_test"
-"rule_rhel810_integrate"
-"rule_before_integrate"
-"rule_tags_permit_scan"
-"rule_cppcheck_scan"
-"rule_x86_clang_tidy_scan"
-"rule_arm_clang_tidy_scan"
-"rule_rhel79_test"
-"rule_rhel86_test"
-"rule_rhel83_test"
-"rule_x86_kylin_v7_test"
-"rule_x86_coverage"
-"rule_arm_kylin_coverage"
-"rule_kylin_v10_test"
-"rule_arm_clang_test"
-"rule_rhel79_integrate"
-"rule_rhel86_integrate"
-"rule_rhel83_integrate"
-"rule_x86_kylin_v7_integrate"
-"rule_kylin_v10_integrate"
-"rule_spec_format"
-"rule_code_docs"
-"rule_delivery"
-"rule_tag_stop"
-)
 
-check_rule_lint() {
+function check_rule_lint() {
   if ([[ -n $CI_COMMIT_BRANCH ]] && [[ -n $GIT_DIFF ]]) || [[ -n $CI_COMMIT_TAG ]]; then
     echo "true" >/tmp/rule_cppcheck_scan
     for image in "${IMAGE_LIST[@]}"; do
@@ -48,37 +55,36 @@ check_rule_lint() {
   fi
 }
 
-check_rule_test() {
+function check_rule_test() {
   if ([[ -n $CI_COMMIT_BRANCH ]] && [[ -n $GIT_DIFF_TEST ]]) || [[ -n $CI_COMMIT_TAG ]]; then
-    echo "有文件变动或者是tag"
     for image in "${IMAGE_LIST[@]}"; do
       case "$image" in
       "rhel7.9")
-        echo "true" >/tmp/rule-rhel7.9-test
+        echo "true" >/tmp/rule_rhel7.9_test
         ;;
       "rhel8.3")
-        echo "true" >/tmp/rule-rhel8.3-test
+        echo "true" >/tmp/rule_rhel8.3_test
         ;;
       "rhel8.6")
-        echo "true" >/tmp/rule-rhel8.6-test
+        echo "true" >/tmp/rule_rhel8.6_test
         ;;
       "rhel8.10")
-        echo "true" >/tmp/rule-rhel8.10-test
+        echo "true" >/tmp/rule_rhel8.10_test
         ;;
       "x86-kylin-v7")
-        echo "true" >/tmp/rule_x86-kylin-v7_test
+        echo "true" >/tmp/rule_x86_kylin_v7_test
         ;;
       "x86-kylin-v10sp1")
-        echo "true" >/tmp/rule_x86-kylin-v10sp1_test
+        echo "true" >/tmp/rule_x86_kylin_v10sp1_test
         ;;
       "x86-kylin-v10sp3")
-        echo "true" >/tmp/rule_x86-kylin-v10sp3_test
+        echo "true" >/tmp/rule_x86_kylin_v10sp3_test
         ;;
       "arm-kylin-v10sp1")
-        echo "true" >/tmp/rule_arm-kylin-v10sp1_test
+        echo "true" >/tmp/rule_arm_kylin_v10sp1_test
         ;;
       "arm-kylin-v10sp3")
-        echo "true" >/tmp/rule_arm-kylin-v10sp3_test
+        echo "true" >/tmp/rule_arm_kylin_v10sp3_test
         ;;
       *)
         echo "检测到未知镜像: $image，不执行特定操作"
@@ -88,50 +94,62 @@ check_rule_test() {
   fi
 }
 
-check_rule_release() {
+function check_rule_release() {
   if [[ $CI_COMMIT_TAG =~ ^([0-9]+\.[0-9]+\.[0-9]+)-.+ ]] || [[ $CI_COMMIT_TAG =~ ^([0-9]+\.[0-9]+\.[0-9]+q)-.+ ]] || [[ $CI_PIPELINE_SOURCE =~ schedule ]]; then
     for image in "${IMAGE_LIST[@]}"; do
       case "$image" in
       "rhel7.9")
-        echo "true" >/tmp/rule-rhel7.9-release
+        echo "true" >/tmp/rule_rhel7.9_integrate
         ;;
       "rhel8.3")
-        echo "true" >/tmp/rule-rhel8.3-release
+        echo "true" >/tmp/rule_rhel8.3_integrate
         ;;
       "rhel8.6")
-        echo "true" >/tmp/rule-rhel8.6-release
+        echo "true" >/tmp/rule_rhel8.6_integrate
         ;;
       "rhel8.10")
-        echo "true" >/tmp/rule-rhel8.10-release
+        echo "true" >/tmp/rule_rhel8.10_integrate
         ;;
       "x86-kylin-v7")
-        echo "true" >/tmp/rule_x86-kylin-v7_release
+        echo "true" >/tmp/rule_x86_kylin_v7_integrate
         ;;
-      "x86-kylin-v10")
-        echo "true" >/tmp/rule_x86-kylin-v10_release
+      "x86-kylin-v10sp1")
+        echo "true" >/tmp/rule_x86_kylin_v10sp1_integrate
+        ;;
+      "x86-kylin-v10sp3")
+        echo "true" >/tmp/rule_x86_kylin_v10sp3_integrate
+        ;;
+      "arm-kylin-v10sp1")
+        echo "true" >/tmp/rule_arm_kylin_v10sp1_integrate
+        ;;
+      "arm-kylin-v10sp3")
+        echo "true" >/tmp/rule_arm_kylin_v10sp3_integrate
+        ;;
+      *)
+        echo "检测到未知镜像: $image，不执行特定操作"
         ;;
       esac
     done
   fi
 }
 
-# 调用生成函数
 main() {
-  # set -e
-  # cd $src
-  # source $env
-  echo $CI_COMMIT_TAG $CI_COMMIT_BRANCH $GIT_DIFF
-  echo "创建参数文件默认赋值为FALSE:"
-  for file in $PARA_FILES; do
-    touch "/tmp/$file" && echo "false" >/tmp/$file # 创建空文件并赋值为false
-  done
-  if [[ -n $CI_COMMIT_BRANCH ]] || [[ -n $CI_COMMIT_TAG ]]; then
-    echo "true" >/tmp/rule_before_integrage
-    echo "true" >/tmp/rule_tags_permit_scan
-  fi
-  check_rule_lint
-  check_rule_test
-  check_rule_release
+set -e
+cd $src
+source $env
+echo "触发TAG/分支：$CI_COMMIT_TAG $CI_COMMIT_BRANCH"
+echo "文件变动涉及：$GIT_DIFF_TEST $GIT_DIFF"
+echo "创建参数文件默认赋值为FALSE:"
+for file in ${PARA_FILES[@]}; do
+  echo $file
+  touch "/tmp/$file" && echo "false" >/tmp/$file # 创建空文件并赋值为false
+done
+if [[ -n $CI_COMMIT_BRANCH ]] || [[ -n $CI_COMMIT_TAG ]]; then
+  echo "true" >/tmp/rule_before_integrate
+  echo "true" >/tmp/rule_tags_permit_scan
+fi
+check_rule_lint
+check_rule_test
+check_rule_release
 }
-
 main
--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "Hello, World1"
-echo "Hello, World2"
-echo "Hello, World3"
-echo "Hello, World4"
-echo "Hello, World5"
+
+function demoFun1(){
+    echo "这是我的第一个 shell 函数!"
+    return 0
+}
+
+demoFun1
